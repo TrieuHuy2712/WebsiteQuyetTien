646235cba6691fba27148151fdc4c259b18f11cb
Huy Trieu <tommy.jimmy2712@gmail.com>
2018-11-21 08:01:36 +0700

    first GUI for pr

---
+++ DienMayWebsiteQuyetTien/DienMayWebsiteQuyetTien/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DienMayWebsiteQuyetTien.Startup))]
+namespace DienMayWebsiteQuyetTien
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
